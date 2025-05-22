ae00edb4827bf175d19d04f10f6b8cf9abc081fb
bekri <babro.koy.kari@gmail.com>
2025-05-22 17:59:00 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// Replace `your-repo-name` with your actual GitHub repo name!
-const repoName = "bekri.github.io";
-
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: `/${repoName}/`,
+  base: '/', // ← ADD THIS LINE
   server: {
     host: "::",
     port: 8080,

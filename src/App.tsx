9f7c551db2129d9b3aab69d643dc2c5d56f74250
bekri <babro.koy.kari@gmail.com>
2025-05-24 16:30:03 +0300

    removed message
    removed message

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
-import Message from "@/pages/Message";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -28,7 +27,6 @@ const App = () => (
             <Route path="/about" element={<About />} />
             <Route path="/portfolio" element={<Portfolio />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/message" element={<Message />} />
             <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>

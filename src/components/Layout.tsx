c7f7ccb5a25178d547e926d9a361826d1ee14fab
bekri <babro.koy.kari@gmail.com>
2025-05-24 16:55:02 +0300

    test redirect from layout
    test redirect from layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,21 @@
-
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
 const Layout = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const redirectPath = params.get("redirect");
+    if (redirectPath) {
+      navigate(redirectPath);
+      // Optionally clean the URL (removes the ?redirect=... part)
+      window.history.replaceState({}, "", window.location.pathname);
+    }
+  }, [navigate]);
+
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
